5f7f7b3da344379dbba31f461eaadf33bf8ad16c
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-02-03 23:38:18 +0600

    database update

--- Database/hospital.sql
+++ Database/hospital.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2023 at 10:21 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Generation Time: Feb 03, 2024 at 06:37 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,8 +34,19 @@ CREATE TABLE `appointment` (
   `phone` varchar(255) NOT NULL,
   `message` text NOT NULL,
   `too` varchar(255) NOT NULL,
-  `cTime` varchar(255) NOT NULL
+  `cTime` varchar(255) NOT NULL,
+  `paymentstatus` varchar(255) DEFAULT NULL,
+  `transID` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `name`, `fromm`, `phone`, `message`, `too`, `cTime`, `paymentstatus`, `transID`) VALUES
+(35, 'Sk. Redwan', '4067', '01521709192', 'hlw', 'e37c742a49ef62f9f9340d7f05be4e30', '1706973888', 'Paid', '65be5ac06bd9d'),
+(36, 'Sk. Redwan', '4067', '01521709192', 'urgent', 'e37c742a49ef62f9f9340d7f05be4e30', '1706974185', 'Paid', '65be5be93863d'),
+(37, 'Sk. Redwan', '4067', '01521709192', 'fever', 'e37c742a49ef62f9f9340d7f05be4e30', '1706974211', 'Paid', '65be5c0361721');
 
 -- --------------------------------------------------------
 
@@ -93,6 +104,24 @@ CREATE TABLE `patients` (
   `pcondition` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`id`, `name`, `location`, `age`, `gender`, `phone`, `dateOfBirth`, `cTime`, `diagnosis`, `prescription`, `token`, `doctor`, `number`, `pcondition`) VALUES
+(1, 'Sk. Redwan', 'Demra, Dhaka', '23', 'Male', '01521709192', '10 - 02 - 2001', '1706942230', 'Heart Block', 'Finix -20mg', 'ec915f6366a3ce25145a507b7df076a5', 'e37c742a49ef62f9f9340d7f05be4e30', '4067', 'Inpatient');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentstatus`
+--
+
+CREATE TABLE `paymentstatus` (
+  `amount` int(11) NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -118,7 +147,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstName`, `secondName`, `email`, `password`, `token`, `status`, `phone`, `profile`, `gender`, `role`) VALUES
-(1, 'E-healthsphare', 'Admin', 'ehealthsphere01@gmail.com', '111111', '1234567890', 1, '0712345678', NULL, 'Male', 'admin');
+(1, 'E-healthsphare', 'Admin', 'ehealthsphere01@gmail.com', '111111', '1234567890', 1, '0712345678', NULL, 'Male', 'admin'),
+(2, 'Sk. Nahid', 'Ahmed', 'nahidsheikh2001@gmail.com', 'hospital', 'e37c742a49ef62f9f9340d7f05be4e30', 2, '01521709192', NULL, 'Male', 'Heart Surgeons');
 
 --
 -- Indexes for dumped tables
@@ -162,7 +192,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `hiv`
@@ -180,13 +210,13 @@ ALTER TABLE `outbreaks`
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
